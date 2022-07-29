e486a8033e40b1173a2cec44ae6e2f92e9bff18a
harmy63 <galmang2015@gmail.com>
2022-07-29 09:59:52 +0900

    upload gradle project

---
+++ src/main/java/com/escape/smkown/SmkownApplication.java
@@ -0,0 +1,13 @@
+package com.escape.smkown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmkownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmkownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escape/smkown/SmkownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escape.smkown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmkownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
